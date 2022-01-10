571426dd46c2f96b81f0f17848ca6b1912d5ac01
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2022-01-10 12:15:34 +0900

    abc234 d

--- problems/abc234/d/d.py
+++ problems/abc234/d/d.py
@@ -1,13 +1,10 @@
+import heapq
 N, K = map(int, input().split())
 P = list(map(int, input().split()))
-
-_P = P[:K]
-sortedP = sorted(_P, reverse=True)
-ans = sortedP[K-1]
-
-for i in range(N-K+1):
-    _P.append(P[K+i])
-    Pi = _P[-1]
-    if (Pi > ans):
-        sortedP[K]
-    print(ans)
+s = P[:K]
+heapq.heapify(s)
+print(min(s))
+for i in range(K, N):
+  heapq.heappush(s, P[i])
+  heapq.heappop(s)
+  print(s[0])
